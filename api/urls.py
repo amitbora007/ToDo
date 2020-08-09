3982ef67fbf7cebcd62aaedcfecf14df62e2220f
Amit Bora <amitbora007@gmail.com>
2020-08-09 16:33:56 +0530

    Delete urls.py

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from api import views
-
-urlpatterns =[
-	url(r'^api', views.tlist),
-	url(r'^api/(?P<pk>[0-9]+)$', views.todo_detail),
-	url(r'^api/admin', views.allUsers_todo),
-]
