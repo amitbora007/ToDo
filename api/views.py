5d8e120956099eefa46806cfe1284ee8fd08db21
Amit Bora <amitbora007@gmail.com>
2020-08-09 16:34:43 +0530

    updated files and removed url.py from here

--- api/views.py
+++ api/views.py
@@ -12,8 +12,8 @@ from rest_framework.decorators import api_view
 # Create your views here.
 @api_view(['GET','POST','DELETE'])
 def tlist(request):
+	todo = ToDo.objects.all()
 	if request.method == 'GET':
-		todo = ToDo.objects.all()
 		todo_serializer = apiSerializer(todo,many=True)
 		return JsonResponse(todo_serializer.data,safe=False)
 			
@@ -24,30 +24,31 @@ def tlist(request):
 			todo_serializer.save()
 			return JsonResponse(todo_serializer.data, status=status.HTTP_201_CREATED)
 		return JsonResponse(todo_serializer.errors, status=status.HTTP_400_BAD_REQUEST)	
+	
 	elif request.method =='DELETE':
-		count = todo.objects.all().delete()
+		count = todo.delete()
 		return JsonResponse(f"{'message:' '{count[0]} Todo deleted successfully'}",status=status.HTTP_204_NO_CONTENT) 
 		
 	
 @api_view(['GET','PUT','DELETE'])	
-def todo_detail(request, pk):
-	try:
-		todo = ToDo.objects.get(pk=pk)
-		if request.method =='GET':
-			todo_serializer = apiSerializer(todo)
-			return JsonResponse(todo_serializer.data)
-		elif request.method == 'PUT':
-			data = JSONParser().parse(request)
-			todo_serializer = apiSerializer(todo, data=data)
-			if todo_serializer.is_valid():
-				todo_serializer.save()
-				return JsonResponse(todo_serializer.data)
-			return JsonResponse(todo_serializer.errors, status=status.HTTP_400_BAD_REQUEST)	
-		elif request.method == 'DELETE':
-			todo.delete()
-			return JsonResponse({'message':'Todo deleted'},status=status.HTTP_204_NO_CONTENT)
-	except	ToDo.DoesNotExist:
-		return JsonResponse({'message':'Nothing Found'}, status=status.HTTP_404_NOT_FOUND)
+def todo_detail(request, id):
+	todo = ToDo.objects.get(pk=id)
+	if request.method =='GET':
+		todo_serializer = apiSerializer(todo)
+		return JsonResponse(todo_serializer.data, safe=False)
+	
+	elif request.method == 'PUT':
+		data = JSONParser().parse(request)
+		todo_serializer = apiSerializer(todo, data=data)
+		if todo_serializer.is_valid():
+			todo_serializer.save()
+			return JsonResponse(todo_serializer.data, safe=False)
+		return JsonResponse(todo_serializer.errors, status=status.HTTP_400_BAD_REQUEST)	
+	
+	elif request.method == 'DELETE':
+		todo.delete()
+		return JsonResponse({'message':'Todo deleted'},status=status.HTTP_204_NO_CONTENT)
+
 		
 
 @api_view(['GET'])	
@@ -56,4 +57,4 @@ def allUsers_todo(request):
 	
 	if request.method== 'GET':
 		todo_serializer = apiSerializer(todo,many=True)
-		return JsonResponse(todo_serializer.data, safe=Flase)
+		return JsonResponse(todo_serializer.data, safe=False)
